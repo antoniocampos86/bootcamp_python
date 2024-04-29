48a7e8eb21ca6b12c12088258e16ef87e0503fe7
Antonio Campos <contato@antoniocampos.com.br>
2024-04-29 11:34:29 -0300

    Adicionando Aula 03,

---
+++ Aula03/main.py
@@ -0,0 +1,23 @@
+# texto = "Hoje é nossa segunda aula de bootcamp , bootcamp de python"
+
+# texto.replace(",", "")
+# texto_tratado = texto.replace(",", "")
+# lista_de_palavras = texto_tratado.split()
+
+# # print(lista_de_palavras)
+# # print(texto.split())
+
+# controle_palavras = {}
+
+# for palavra in lista_de_palavras:
+#     if palavra in controle_palavras:
+#         controle_palavras[palavra] +=1
+#     else:
+#         controle_palavras[palavra] = 1
+# print(controle_palavras)
+
+import time
+
+while True:
+    print("Oi")
+    time.sleep(3)
---
+++ Aula03/teste/guanabara.py
@@ -0,0 +1,19 @@
+pessoas = {'nome': 'Antonio', 'sexo': 'M', 'idade': 23}
+count = 0
+# print(pessoas)
+# print(f"chaves: {pessoas.keys()}")
+print(f"valores: {pessoas.values()}")
+print(f"itens: {pessoas.items()}")
+# print(f"O nome é {pessoas['nome']}")
+
+# for k in pessoas.keys():
+#     # print(k)
+#     count = count + 1
+#     print(f"{k} é a {count}a chave.")
+# for k,v in pessoas.items():
+#     print(f"{k} = {v}")
+# del pessoas['idade']
+# pessoas['peso'] = 113
+# print("------------------------------------")
+# for k,v in pessoas.items():
+#     print(f"{k} = {v}")
