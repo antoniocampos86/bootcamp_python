07287f9bc77d99e3f79691a10b3bf53ce27460f8
Antonio Campos <contato@antoniocampos.com.br>
2024-04-30 14:07:19 -0300

    Exemplos de dicionarios

---
+++ Aula04/exemplo_dicionario.py
@@ -0,0 +1,24 @@
+pessoa = dict()
+
+pessoa = {
+    "Nome": "Antonio",
+    "Idade": 38,
+    "Cargo": "Engenheiro de Dados",
+    "Setor": "TI"
+}
+
+pessoa2 = {
+    "Nome": "João",
+    "Idade": 50,
+    "Cargo": "Engenheiro Civil",
+    "Setor": "Obras"
+}
+
+
+funcionarios = list()
+
+# Adicionando pessoas a lista de funcionários
+funcionarios.append(pessoa)
+funcionarios.append(pessoa2)
+
+print(funcionarios)
