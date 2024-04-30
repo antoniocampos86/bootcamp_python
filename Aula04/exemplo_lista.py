526e5f8108dc1fcb1a535ea8859941c862b003fc
Antonio Campos <contato@antoniocampos.com.br>
2024-04-30 13:47:34 -0300

    exemplo de lista

---
+++ Aula04/exemplo_lista.py
@@ -0,0 +1,18 @@
+# Exemplos de Type HInt:
+idade: int = 0
+preco: float = 5.6
+nome: str = "Fatima"
+status: bool = True
+
+
+teste = list()
+
+teste.append(nome)
+teste.append("Antonio")
+teste.append("Rafaela")
+teste.append("Yasmin")
+# teste.pop()
+# teste.copy()
+# teste.count()
+
+print(teste)
